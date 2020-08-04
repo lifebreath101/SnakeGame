094cfd339ae5e72baf6a91d9666b3349d9067271
System Administrator <root@Ds-MacBook-Pro.local>
2020-08-03 21:37:59 -0700

    learning about java2dAPI

--- IdeaProjects/SnakeGame1.01/src/SnakeWindow.java
+++ IdeaProjects/SnakeGame1.01/src/SnakeWindow.java
@@ -1,5 +1,49 @@
-public class SnakeWindow {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.font.TextLayout;
+
+class Test extends JFrame {
+
+    static JFrame testFrame;
+
+    static JPanel testFramePanel;
+
+    //static JFrame testFrameText;
+
+    static JButton testFrameButton;
+
+
+    public static void main(String[] args) {
+
+        testFrame = new JFrame("will this frame work?");
+
+        testFramePanel = new JPanel();
+
+        testFrameButton = new JButton("I guess it will!");
+
+        testFramePanel.add(testFrameButton);
+
+        //testFrame.getContentPane().add(testFrameText);
+
+        testFramePanel.setBackground(Color.white);
+
+        testFrame.add(testFramePanel);
+
+        testFrame.setSize(300, 300);
+
+        testFrame.setVisible(true);
+
+        System.out.println(testFrame.isShowing());
+
+    }
 }
+
+
+
+//In this example I created a graphic window and added a button to it
+
+//TODO: make objects and "raw" text to the window
+
 
 
 
